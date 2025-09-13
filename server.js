881114ffc8b51c1ba6c42068c743ea1cc7095d8a
Upasana Saha <sahaupasana2003@gmail.com>
2025-09-14 00:06:56 +0530

    Server.js file updated

---
+++ server.js
@@ -0,0 +1,29 @@
+const express= require('express')
+const cors=require('cors')
+const morgan=require('morgan')
+const dotenv=require('dotenv')
+const colors=require('colors')
+
+//congig the .dot env file 
+dotenv.config()
+
+//rest object 
+const app=express()
+
+//middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+//routes 
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello from server</h1>")
+})
+
+//port 
+const PORT = 8080 || process.env.PORT
+
+//listen server 
+app.listen(PORT , () =>{
+console.log(`server running on port ${PORT}`)
+})
